b16cacf90332e1ca7062ae6d81f4f3ff183ceb38
jordan-strong <jstrong3@byu.edu>
2019-01-17 14:52:07 -0700

    added the square dialoghelper class

--- src/android/squarePOSplugin.java
+++ src/android/squarePOSplugin.java
@@ -18,11 +18,61 @@ import android.os.Bundle;
 import android.content.ActivityNotFoundException;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 
 import com.squareup.sdk.pos.ChargeRequest;
 import com.squareup.sdk.pos.CurrencyCode;
 import com.squareup.sdk.pos.PosClient;
 import com.squareup.sdk.pos.PosSdk;
+
+public class AlertDialogHelper extends DialogFragment {
+
+      private static AlertDialog mAlertDialog;
+
+      @Override
+      public Dialog onCreateDialog(Bundle savedInstanceState) {
+        super.onCreateDialog(savedInstanceState);
+        return null;
+      }
+
+      private static Dialog getDialog(Activity activity,
+        String title, String description, int resourceId) {
+        if (mAlertDialog == null) {
+          mAlertDialog =  new AlertDialog.Builder(activity, resourceId)
+              .setTitle(title + ": " + description)
+              .setPositiveButton("OK", null)
+              .create();
+        } else {
+          mAlertDialog.setTitle(title + ":" + description);
+          mAlertDialog.setOwnerActivity(activity);
+        }
+        return mAlertDialog;
+      }
+
+      public static void showDialog(Activity activity, String title,
+         String description) {
+           int resourceId = 0;
+           try {
+             resourceId =
+             activity
+                  .getPackageManager()
+                  .getActivityInfo(activity.getComponentName()
+                      , 0)
+                  .getThemeResource();
+          } catch (PackageManager.NameNotFoundException e) {
+          e.printStackTrace();
+          } finally {
+            getDialog(
+              activity,
+              title,
+              description,
+              resourceId).show();
+            }
+      }
+}
 
 public class squarePOSplugin extends CordovaPlugin {
   private static final String APPLICATION_ID = "sq0idp-qHqpaVYtEOSxH1Kz6IODFw";
