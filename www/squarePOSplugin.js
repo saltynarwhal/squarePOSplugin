fb05aaae5adfcb1663ca79b08f754eaa99fecdf4
jordan-strong <jstrong3@byu.edu>
2019-01-23 11:22:12 -0700

    edited java connections

--- www/squarePOSplugin.js
+++ www/squarePOSplugin.js
@@ -3,19 +3,8 @@ function squarePOSplugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-squarePOSplugin.prototype.startTransaction = function(successCallback, errorCallback) {
+squarePOSplugin.startTransaction = function(successCallback, errorCallback) {
   cordova.exec(successCallback, errorCallback, 'squarePOSplugin', 'startTransaction');
-}
-
-squarePOSplugin.prototype.onActivityResult = function(resultObject, successCallback, errorCallback) {
-
-  var args = [
-    resultObject.requestCode,
-    resultObject.resultCode,
-    resultObject.data
-  ];
-
-  cordova.exec(successCallback, errorCallback, 'squarePOSplugin', 'onActivityResult', args);
 }
 
 // Installation constructor that binds ToastyPlugin to window
