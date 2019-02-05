9855e5a8b41096db29303e188b6c679f675c1981
saltynarwhal <design.jstrong@gmail.com>
2019-02-05 16:15:37 -0700

    added the request to navigate to the Square POS app ios

--- src/ios/squarePOSplugin.m
+++ src/ios/squarePOSplugin.m
@@ -18,7 +18,7 @@
 
     // Your client ID is the same as your Square Application ID.
     // Note: You only need to set your client ID once, before creating your first request.
-    [SCCAPIRequest setClientID:YOUR_CLIENT_ID];
+    [SCCAPIRequest setClientID:"sq0idp-LtAn6a920ToNj7R4TcKrFA"];
 
     SCCAPIRequest *request = [SCCAPIRequest requestWithCallbackURL:callbackURL
                                         amount:amount
@@ -30,6 +30,39 @@
                                         clearsDefaultFees:NO
                                         returnAutomaticallyAfterPayment:NO
                                         error:&error];
+
+    //send the transaction to the Square Point of Sale app
+    BOOL success = [SCCAPIConnection performRequest:request error:&error];
+
 }
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options; {
+    NSString *const sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey];
+    // Make sure the URL comes from Square Point of Sale; fail if it doesn't.
+    if (![sourceApplication hasPrefix:@"com.squareup.square"]) {
+        return NO;
+    }
+
+    // The response data is encoded in the URL and can be decoded as an SCCAPIResponse.
+    NSError *decodeError = nil;
+    SCCAPIResponse[] *const response = [SCCAPIResponse responseWithResponseURL:url error:&decodeError];
+
+    if (response.isSuccessResponse) {
+      //Print checkout object
+      NSLog(@"Transaction successful: %@", response);
+
+    } else if (decodeError) {
+      //Print decode error
+        NSLog(@"Decode Error: %@", decodeError);
+    }
+    else {
+      //Print the error code
+        NSLog(@"Request failed: %@", response.error);
+    }
+
+    return YES;
+}
+
+
 
 @end
