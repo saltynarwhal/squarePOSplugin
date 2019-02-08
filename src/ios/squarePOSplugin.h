bf8fef1c7dd29eb1bbea542962c0e9df63b96ac3
jordan-strong <jstrong3@byu.edu>
2019-02-08 14:15:48 -0700

    revert

--- src/ios/squarePOSplugin.h
+++ src/ios/squarePOSplugin.h
@@ -5,6 +5,5 @@
 @interface squarePOSplugin : CDVPlugin
 
 - (void)startTransaction:(CDVInvokedUrlCommand*)command;
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options;
 
 @end
