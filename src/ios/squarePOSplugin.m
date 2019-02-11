dc2efa6b666763fa9483a6e7501e75c9854a9cba
jordan-strong <jstrong3@byu.edu>
2019-02-11 11:08:14 -0700

    Reverting to original settings to try and figure out the build error

--- src/ios/squarePOSplugin.m
+++ src/ios/squarePOSplugin.m
@@ -8,15 +8,10 @@
 
 - (void)startTransaction:(CDVInvokedUrlCommand*)command {
   CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
+  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
