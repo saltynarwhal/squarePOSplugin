228f3a1dbc07be16651204f53747a37627d79a6e
jordan-strong <jstrong3@byu.edu>
2019-02-11 10:52:20 -0700

    plugin test

--- src/ios/squarePOSplugin.m
+++ src/ios/squarePOSplugin.m
@@ -8,68 +8,17 @@
 @implementation squarePOSplugin
 
 
-- (void)startTransaction:(CDVInvokedUrlCommand*)command {/*
-    // Replace with your app's callback URL.
-    // Note: You can retrieve this value from Info.plist
-    CDVPluginResult *pluginResult = nil;
-    NSString *squarePOSpluginURL = @"squarePOSplugin://";
+- (void)startTransaction:(CDVInvokedUrlCommand*)command {
+  CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
 
-    NSURL *const callbackURL = [NSURL URLWithString:squarePOSpluginURL];
-
-    NSInteger *argsAmount = [command.arguments objectAtIndex:1];
-
-
-    // Specify the amount of money to charge.
-    SCCMoney *const amount = [SCCMoney moneyWithAmountCents:argsAmount currencyCode:@"USD" error:NULL];
-
-    // Your client ID is the same as your Square Application ID.
-    // Note: You only need to set your client ID once, before creating your first request.
-    [SCCAPIRequest setClientID:"sq0idp-LtAn6a920ToNj7R4TcKrFA"];
-
-    SCCAPIRequest *request = [SCCAPIRequest requestWithCallbackURL:callbackURL
-                                        amount:amount
-                                        userInfoString:nil
-                                        merchantID:nil
-                                        notes:@"Coffee"
-                                        customerID:nil
-                                        supportedTenderTypes:SCCAPIRequestTenderTypeAll
-                                        clearsDefaultFees:NO
-                                        returnAutomaticallyAfterPayment:NO
-                                        error:&error];
-
-    //send the transaction to the Square Point of Sale app
-    BOOL success = [SCCAPIConnection performRequest:request error:&error];
-}
-
-//open the original application and process the result of the square POS
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options; {
-    NSString *const sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey];
-    // Make sure the URL comes from Square Point of Sale; fail if it doesn't.
-    if (![sourceApplication hasPrefix:@"com.squareup.square"]) {
-        return NO;
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
 
-    // The response data is encoded in the URL and can be decoded as an SCCAPIResponse.
-    NSError *decodeError = nil;
-    SCCAPIResponse[] *const response = [SCCAPIResponse responseWithResponseURL:url error:&decodeError];
-
-    if (response.isSuccessResponse) {
-      //Print checkout object
-      NSLog(@"Transaction successful: %@", response);
-      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:options.serverTransactionId];
-
-    } else if (decodeError) {
-      //Print decode error
-        NSLog(@"Decode Error: %@", decodeError);
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Decode Error"];
-    }
-    else {
-      //Print the error code
-        NSLog(@"Request failed: %@", response.error);
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Request failed"];
-    }
-
-    return YES;*/
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
