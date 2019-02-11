c08b2817f8052d4e2908cd31ebf710a3fc502561
jordan-strong <jstrong3@byu.edu>
2019-02-11 16:55:25 -0700

    dasf

--- src/ios/squarePOSplugin.m
+++ src/ios/squarePOSplugin.m
@@ -1,21 +1,28 @@
 /********* squarePOSplugin.m Cordova Plugin Implementation *******/
 
 #import "squarePOSplugin.h"
-#import <Cordova/CDVPlugin.h>
-#import <Cordova/CDVAvailability>
+
+@interface squarePOSplugin ()
+
+typedef enum {
+    ERROR_INVALID_ARGS = 0,
+    ERROR_USER_CANCEL = 1,
+    ERROR_CANT_READ_CARD = 2
+} OSSquareError;
+
+@property (strong, nonatomic) NSString* callbackId;
+
+@end
 
 @implementation squarePOSplugin
 
-- (void)pluginInitialize {
-
-}
-
 - (void)startTransaction:(CDVInvokedUrlCommand*)command {
   CDVPluginResult* pluginResult = nil;
+  self.callbackId = command.callbackId;
 
   pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"worked"];
 
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId;
 }
 
 @end
