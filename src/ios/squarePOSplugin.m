e1050fd9eed38fbdddcc0b4f6a78b8c26e2b9fce
jordan-strong <jstrong3@byu.edu>
2019-02-11 17:00:09 -0700

    sdaf

--- src/ios/squarePOSplugin.m
+++ src/ios/squarePOSplugin.m
@@ -4,19 +4,13 @@
 
 @interface squarePOSplugin ()
 
-typedef enum {
-    ERROR_INVALID_ARGS = 0,
-    ERROR_USER_CANCEL = 1,
-    ERROR_CANT_READ_CARD = 2
-} OSSquareError;
-
 @property (strong, nonatomic) NSString* callbackId;
 
 @end
 
 @implementation squarePOSplugin
 
-- (void)startTransaction:(CDVInvokedUrlCommand*)command {
+- (void) startTransaction:(CDVInvokedUrlCommand*) command {
   CDVPluginResult* pluginResult = nil;
   self.callbackId = command.callbackId;
 
