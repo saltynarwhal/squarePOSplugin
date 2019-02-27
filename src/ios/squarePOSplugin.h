9a03137a3f390e3bea64d385204cf0d222750f63
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-02-27 17:11:30 +0000

    Fix squarePOSplugin header

--- src/ios/squarePOSplugin.h
+++ src/ios/squarePOSplugin.h
@@ -1,9 +1,11 @@
-/********* squarePOSplugin.h Cordova Plugin Header *******/
-
 #import <Cordova/CDVPlugin.h>
+
+#import <SquarePointOfSaleSDK.h>
 
 @interface squarePOSplugin : CDVPlugin
 
-- (void)startTransaction:(CDVInvokedUrlCommand*)command; 
+- (void)startTransaction:(CDVInvokedUrlCommand*)command;
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options;
 
 @end
