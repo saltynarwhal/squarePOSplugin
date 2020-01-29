58c0564f931f50e1491e5e60d03faf5796832082
saltynarwhal <saltynarwhal@users.noreply.github.com>
2020-01-29 10:42:00 -0700

    test ios

--- src/ios/squarePOSplugin.m
+++ src/ios/squarePOSplugin.m
@@ -54,12 +54,13 @@ NSMutableDictionary *options;
 
     //Send plugin result
     //[self.commandDelegate sendPluginResult:pluginResult callbackId:callbackID];
+    CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+      messageAsString:@"Error Message";
+    [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
     //send the transaction to the Square Point of Sale app
     BOOL success = [SCCAPIConnection performRequest:request error:&error];
     if (!success) {
-      CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-        messageAsString:@"Error Message";
-      [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
+
     }
 
     //CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
