e35d5241014e37c439dd91c5173914493f36c323
saltynarwhal <saltynarwhal@users.noreply.github.com>
2020-01-29 15:31:27 -0700

    adsf

--- src/ios/squarePOSplugin.h
+++ src/ios/squarePOSplugin.h
@@ -8,4 +8,7 @@
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options;
 
++ (void)didFinishLaunching:(NSNotification*)notification;
+
++(void)load; 
 @end
