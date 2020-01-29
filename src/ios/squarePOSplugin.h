31f99a9e51fec619ef964231d6248f8ceffb6cf6
saltynarwhal <saltynarwhal@users.noreply.github.com>
2020-01-29 12:01:37 -0700

    attempt

--- src/ios/squarePOSplugin.h
+++ src/ios/squarePOSplugin.h
@@ -6,6 +6,6 @@
 
 - (void)startTransaction:(CDVInvokedUrlCommand*)command;
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options;
+//- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options;
 
 @end
