0bf12ba859db3e46e469a0e76e1e56ca7ba9898a
saltynarwhal <saltynarwhal@users.noreply.github.com>
2020-01-29 12:15:50 -0700

    added different URL handler

--- src/ios/squarePOSplugin.m
+++ src/ios/squarePOSplugin.m
@@ -55,7 +55,7 @@ NSMutableDictionary *options;
     }
 
     //Send plugin result
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackID];
+    //[self.commandDelegate sendPluginResult:pluginResult callbackId:callbackID];
 
 
 }
@@ -98,6 +98,46 @@ NSMutableDictionary *options;
     }
 
     return YES;
+}
+
+func application(_ application: UIApplication,
+                 open url: URL,
+                 options: [UIApplicationOpenURLOptionsKey : Any] = [:] ) -> Bool {
+                   NSString *const sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey];
+                   // Make sure the URL comes from Square Point of Sale; fail if it doesn't.
+                   //if (![sourceApplication hasPrefix:@"com.squareup.square"]) {
+                   //    return NO;
+                   //}
+
+                   // The response data is encoded in the URL and can be decoded as an SCCAPIResponse.
+                   NSError *decodeError = nil;
+                   SCCAPIResponse *const response = [SCCAPIResponse responseWithResponseURL:url error:&decodeError];
+
+                   if (response.isSuccessResponse) {
+                     NSLog(@"Transaction successful: %@", response);
+                       CDVPluginResult *successPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                           messageAsString:response.transactionID];
+                       [self.commandDelegate sendPluginResult:successPlugin callbackId:callbackID];
+
+                   } else if (decodeError) {
+                       //Print decode error
+                       NSLog(@"Decode Error: %@", decodeError);
+
+                       CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                           messageAsString:[decodeError localizedDescription]];
+                       [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
+                   }
+                   else {
+                       //Print the error code
+                       NSLog(@"Request failed: %@", response.error);
+                       CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                           messageAsString:[response.error localizedDescription]];
+                       [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
+
+                   }
+
+                   return YES;
+
 }
 
 @end
