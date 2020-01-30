72fbd984432b226ec6fc6ed5fc41c1fedc6081c8
saltynarwhal <saltynarwhal@users.noreply.github.com>
2020-01-30 11:05:51 -0700

    Running into compile errors on AppDelegate+squarePOSplugin.m

--- src/ios/AppDelegate+squarePOSplugin.m
+++ src/ios/AppDelegate+squarePOSplugin.m
@@ -1,6 +1,8 @@
 #import "AppDelegate+squarePOSplugin.h"
 #import "squarePOSplugin.h"
 
+@interface CDVAppDelegate ()
+
 @implementation CDVAppDelegate
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options
