2a80c9e6bf64741a760ae42fc7e7fd53dec4bda3
saltynarwhal <saltynarwhal@users.noreply.github.com>
2020-01-31 16:34:21 -0700

    test

--- src/ios/squarePOSplugin.m
+++ src/ios/squarePOSplugin.m
@@ -12,6 +12,45 @@
 NSString *callbackID;
 NSMutableDictionary *options;
 
+- (void)pluginInitialize
+{
+  - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options
+  {
+      NSString *const sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey];
+      // Make sure the URL comes from Square Point of Sale; fail if it doesn't.
+      if (![sourceApplication hasPrefix:@"com.squareup.square"]) {
+          return NO;
+      }
+
+      // The response data is encoded in the URL and can be decoded as an SCCAPIResponse.
+      NSError *decodeError = nil;
+      SCCAPIResponse *const response = [SCCAPIResponse responseWithResponseURL:url error:&decodeError];
+
+      if (response.isSuccessResponse) {
+          CDVPluginResult *successPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+              messageAsString:response.transactionID];
+          [self.commandDelegate sendPluginResult:successPlugin callbackId:callbackID];
+
+      } else if (decodeError) {
+          //Print decode error
+          NSLog(@"Decode Error: %@", decodeError);
+
+          CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+              messageAsString:[decodeError localizedDescription]];
+          [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
+      }
+      else {
+          //Print the error code
+          NSLog(@"Request failed: %@", response.error);
+          CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+              messageAsString:[response.error localizedDescription]];
+          [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
+      }
+
+      return YES;
+  }
+}
+
 - (void)startTransaction:(CDVInvokedUrlCommand*)command {
     callbackID = command.callbackId;
     options = [command.arguments objectAtIndex:0];
@@ -53,7 +92,7 @@ NSMutableDictionary *options;
 
 }
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options
+/*- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options
 {
     NSString *const sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey];
     // Make sure the URL comes from Square Point of Sale; fail if it doesn't.
@@ -87,6 +126,6 @@ NSMutableDictionary *options;
     }
 
     return YES;
-}
+}*/
 
 @end
