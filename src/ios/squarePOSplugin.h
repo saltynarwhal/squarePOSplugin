489934e5cbcf65ea2c7c3fc12034887f88f40e67
saltynarwhal <saltynarwhal@users.noreply.github.com>
2020-01-31 16:04:47 -0700

    test

--- src/ios/squarePOSplugin.h
+++ src/ios/squarePOSplugin.h
@@ -3,14 +3,10 @@
 #import <SquarePointOfSaleSDK.h>
 #import <UIKit/UIKit.h>
 
-@protocol UIApplicationDelegate
-
-  - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options;
-
-@end
-
 @interface squarePOSplugin : CDVPlugin <UIApplicationDelegate>
 
   - (void)startTransaction:(CDVInvokedUrlCommand*)command;
+
+    - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options;
 
 @end
