15dffcdaeff0d2254c90c56a358df2cbc567b54e
saltynarwhal <design.jstrong@gmail.com>
2020-02-03 12:05:39 -0700

    add

--- src/ios/customAppDelegate.m
+++ src/ios/customAppDelegate.m
@@ -1,5 +1,6 @@
 #import "customAppDelegate.h"
 
+@UIApplicationMain
 @implementation squarePOSpluginDelegate
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options
