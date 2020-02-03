3d600ffd8a4b3cbd9f720b5a412b8d4f3189ebfe
saltynarwhal <design.jstrong@gmail.com>
2020-02-03 11:32:58 -0700

    test

--- src/ios/squarePOSplugin.m
+++ src/ios/squarePOSplugin.m
@@ -21,41 +21,7 @@ NSMutableDictionary *options;
 
 - (void)finishLaunching:(NSNotification *)notification
 {
-  - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options
-  {
-      NSString *const sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey];
-      // Make sure the URL comes from Square Point of Sale; fail if it doesn't.
-      if (![sourceApplication hasPrefix:@"com.squareup.square"]) {
-          return NO;
-      }
-
-      // The response data is encoded in the URL and can be decoded as an SCCAPIResponse.
-      NSError *decodeError = nil;
-      SCCAPIResponse *const response = [SCCAPIResponse responseWithResponseURL:url error:&decodeError];
-
-      if (response.isSuccessResponse) {
-          CDVPluginResult *successPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-              messageAsString:response.transactionID];
-          [self.commandDelegate sendPluginResult:successPlugin callbackId:callbackID];
-
-      } else if (decodeError) {
-          //Print decode error
-          NSLog(@"Decode Error: %@", decodeError);
-
-          CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-              messageAsString:[decodeError localizedDescription]];
-          [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
-      }
-      else {
-          //Print the error code
-          NSLog(@"Request failed: %@", response.error);
-          CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-              messageAsString:[response.error localizedDescription]];
-          [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
-      }
-
-      return YES;
-  }
+    // Put here the code that should be on the AppDelegate.m
 }
 
 - (void)startTransaction:(CDVInvokedUrlCommand*)command {
@@ -97,6 +63,42 @@ NSMutableDictionary *options;
     //send the transaction to the Square Point of Sale app
     BOOL success = [SCCAPIConnection performRequest:request error:&error];
 
+}
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options
+{
+    NSString *const sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey];
+    // Make sure the URL comes from Square Point of Sale; fail if it doesn't.
+    if (![sourceApplication hasPrefix:@"com.squareup.square"]) {
+        return NO;
+    }
+
+    // The response data is encoded in the URL and can be decoded as an SCCAPIResponse.
+    NSError *decodeError = nil;
+    SCCAPIResponse *const response = [SCCAPIResponse responseWithResponseURL:url error:&decodeError];
+
+    if (response.isSuccessResponse) {
+        CDVPluginResult *successPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+            messageAsString:response.transactionID];
+        [self.commandDelegate sendPluginResult:successPlugin callbackId:callbackID];
+
+    } else if (decodeError) {
+        //Print decode error
+        NSLog(@"Decode Error: %@", decodeError);
+
+        CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+            messageAsString:[decodeError localizedDescription]];
+        [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
+    }
+    else {
+        //Print the error code
+        NSLog(@"Request failed: %@", response.error);
+        CDVPluginResult *errorPlugin = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+            messageAsString:[response.error localizedDescription]];
+        [self.commandDelegate sendPluginResult:errorPlugin callbackId:callbackID];
+    }
+
+    return YES;
 }
 
 @end
